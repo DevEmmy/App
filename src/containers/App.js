921d5f4498b60cda052595da055516ef539f9e2d
Fawaz <fawazadeniji123@gmail.com>
2021-08-31 13:39:24 +0100

    adding component --fawaz

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,12 @@
+import Signin from '../components/Signin/Signin';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>SKISH-NG</h1>
+      <Signin />
     </div>
   );
 }
