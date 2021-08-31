51b968c4d25a0d119edf73c2ed8e945b57a78572
Fawaz <fawazadeniji123@gmail.com>
2021-08-31 14:51:40 +0100

    adding components --fawaz

---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,26 @@
+// import './Signin.css';
+
+function Nav() {
+
+    return (
+        <nav>
+            <div>
+                <img />
+                <div>
+                    <h2>John Doe</h2>
+                    <p>johndoe@gmail.com</p>
+                </div>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>My Account</li>
+                <li>Saved Artian</li>
+                <li>About</li>
+                <li>Support</li>
+                <li>Log Out</li>
+            </ul>
+        </nav>
+    );
+}
+  
+export default Nav;
